eb0e7ac0f88998b34f3820763f2c9c83a1e5c3ac
Michael Smith <michael.smith@vmlyr.com>
2023-07-16 19:39:38 -0500

    add copy for dress code.

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
             <div><a href="https://www.google.com/maps/place/Grand+Street+Cafe/@39.0413377,-94.5880478,17z/data=!3m1!4b1!4m6!3m5!1s0x87c0efbfabd07121:0x4d29f6c1222606e7!8m2!3d39.0413336!4d-94.5854675!16s%2Fg%2F1tfzbq84?entry=ttu" target="_blank">4740 Grand Ave &#x2022; Kansas City, MO &#x2022; 64112</a></div>
           </div>
           <div className="header__details">
-            <div><a href="https://www.brides.com/black-tie-optional-wedding-attire-4800698" target="_blank">Black Tie Optional</a></div>
+            <div>Dress code is <a href="https://www.brides.com/black-tie-optional-wedding-attire-4800698" target="_blank">Black Tie Optional</a></div>
           </div>
         </Section>
 
