95d4a74b59f2dab4f63cd0cd81e3b851f23b8f52
Irina Usman <irinausman@gmail.com>
2023-07-24 17:02:30 -0500

    added about Michael, changed About Us section

--- components/Sections/OurStory.js
+++ components/Sections/OurStory.js
@@ -11,17 +11,26 @@ export default function OurStory() {
       <IndividualStory title="Irina" img="/irina1.jpeg" img_alt="">
         <p>Introducing Michael's incredible fiancée, Irina – the love of his life and his ultimate best friend.</p>
         <p>Originally from Ukraine, Irina has embraced her new life as a US citizen, having resided here for almost as long as she did in her home country. While she has created a beautiful life alongside Michael in Kansas City, she remains deeply connected to her roots, with her brother, sister-in-law, father, and childhood best friend all residing here and cherishing their Ukrainian traditions.</p>
-        <p>An extraordinary aspect of their relationship is the professional collaboration they share. Irina and Michael work side by side at the same company, often collaborating on joint projects. It was Irina's boldness that sparked their romantic journey when she took the initiative to ask Michael out. Since that moment, their lives have been filled with joy and fulfillment.</p>
-
+        <p>Irina has a 13 year old son Sasha, who is extremely excited about Michael officially becoming a part of their family, although for Sasha, Michael is already a family by being patient, understanding and supportive role model.</p>
+<p>Irina is looking forward to marrying Michael and share a happy life together. </p>
       </IndividualStory>
 
       <IndividualStory title="Michael" img="/michael1.jpeg" img_alt="">
-        <p>Michael </p>
+        <p>Introducing the remarkable man who has captured Irina's heart and is set to become her loving partner in life - Michael.</p>
+<p>From the moment Michael entered Irina's life, it was evident that he was someone truly special.</p>
+<p>Not only does Michael have the kindest heart, but he also has a keen intellect and a passion for knowledge. He approaches life with a curious and open mind, always seeking to broaden his horizons and learn from every experience. Michael's love for adventure and exploration perfectly complements Irina's adventurous spirit. Together, they embrace the beauty of life, explore the world to create unforgettable  memories in every moment they share.</p>
+<p>It is with great excitement that Michael is anticipating to build a beautiful family and happy future that he dreams of together with Irina. He is sure hat their future together holds nothing but happiness and fulfillment.</p>
+ 
       </IndividualStory>
 
       <TogetherStory title="Us Together!" img="/us1.jpeg" img_alt="">
-        <p>As they stand on the precipice of their wedding day, Michael and Irina are filled with excitement for the future. They look forward to continuing their journey as partners, building a life rooted in love, understanding, and shared dreams. With hearts intertwined and a steadfast commitment, they eagerly anticipate the beautiful chapters yet to be written, knowing that their love will guide them through every moment, now and forever.</p>
-      </TogetherStory>
+        <p>Every story has a beginning. This one started unexpectedly for both. Have you heard of the Office Romance? Yeap, that is where it started.</p>
+        <p>Irina and Michael work side by side at the same company. During collaboration on a project, Irina was tired uselessly smiling at Michael and sent an email asking him out. A few months have passed before the first date happened. That day marks the beginning of the beautiful journey.</p>
+        <p>They started spending all of their free time together: hiking, playing tennis, watching movies, camping,  cooking, hosting parties… Time was flying by. It was time to expand the horizons.</p>
+        <p>The first trip they took together was during the Christmas holidays. What is better than a sunny beach of Mexico during cold winter in Missouri?! Michael, Irina and Sasha enjoyed the time exploring this amazing country. It was another milestone in their life together. Traveling became a part of the relationships. Los Angeles, Herman, San Diego, islands of Belize, Guatemala is just a beginning of the world exploration.</p>
+        <p>Two-year dating mark was checked and Michael decided to take the relationships to the next level. He bought a ring! Before asking Irina’s hand in marriage, Michael asked Sasha, his future step son, for “permission”. Sasha approved! It was time to plan the proposal. With the help of friends and family, Michael proposed in Powell Gardens (one of Irina’s favorite places). The engagement surprise party was waiting for them at home. It was perfect!</p> 
+        <p>Now, as they stand on the precipice of their wedding day, Michael and Irina are filled with excitement for the future. They look forward to continuing their journey as partners, building a life rooted in love, understanding, and shared dreams. With hearts intertwined and a steadfast commitment, they eagerly anticipate the beautiful chapters yet to be written, knowing that their love will guide them through every moment, now and forever.</p>
+             </TogetherStory>
     </div>
     </Section>
   )
